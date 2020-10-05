c0d41ef3bba64ef63058f31967647dcf9be3bb4a
zendzo <zendzzo@gmail.com>
2020-10-05 10:41:32 +0700

    create product_sale_table for many to many relationship

---
+++ database/migrations/2020_10_04_161516_create_product_sale_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductSaleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_sale', function (Blueprint $table) {
+            $table->id();
+            $table->integer('sale_id');
+            $table->integer('product_id');
+            $table->integer('quantity');
+            $table->integer('grand_total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_sale');
+    }
+}
