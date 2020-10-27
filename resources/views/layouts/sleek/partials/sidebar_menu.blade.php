40b31cdc3e5d7e46c32fea35f88c1de0267aada8
zendzo <zendzzo@gmail.com>
2020-10-27 10:56:08 +0700

    add active status on each sidebar menu

--- resources/views/layouts/sleek/partials/sidebar_menu.blade.php
+++ resources/views/layouts/sleek/partials/sidebar_menu.blade.php
@@ -3,23 +3,23 @@
 
   <!-- sidebar menu -->
   <ul class="nav sidebar-inner" id="sidebar-menu">
-    <li class="has-sub">
+    <li class="has-sub {{ active(['admin.sales', 'admin.purchase'], 'expand active') }}">
       <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#dashboard"
         aria-expanded="false" aria-controls="dashboard">
         <i class="mdi mdi-cart"></i>
         <span class="nav-text">Transaksi</span> <b class="caret"></b>
       </a>
-      <ul class="collapse" id="dashboard" data-parent="#sidebar-menu">
+      <ul class="collapse {{ active(['admin.sales', 'admin.purchase'], 'show') }}" id="dashboard" data-parent="#sidebar-menu">
         <div class="sub-menu">
 
-          <li>
+          <li class="{{ active(['admin.sales'], 'active') }}">
             <a class="sidenav-item-link" href="{{ route('admin.sales') }}">
               <span class="nav-text">Penjualan</span>
               <span class="badge badge-warning">OUT</span>
             </a>
           </li>
 
-          <li>
+          <li class="{{ active(['admin.purchase'], 'active') }}">
             <a class="sidenav-item-link" href="{{ route('admin.purchase') }}">
               <span class="nav-text">Pembelian</span>
               <span class="badge badge-success">IN</span>
@@ -29,51 +29,51 @@
         </div>
       </ul>
     </li>
-    <li class="has-sub">
+    <li class="has-sub {{ active(['admin.category', 'admin.product', 'admin.unit', 'admin.sales-type', 'admin.purchase-type', 'admin.supplier'], 'expand active') }}">
       <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#app"
         aria-expanded="false" aria-controls="app">
         <i class="mdi mdi-pencil-box-multiple"></i>
         <span class="nav-text">MASTER DATA</span> <b class="caret"></b>
       </a>
-      <ul class="collapse" id="app" data-parent="#sidebar-menu">
+      <ul class="collapse {{ active(['admin.category', 'admin.product', 'admin.unit', 'admin.sales-type', 'admin.purchase-type', 'admin.supplier'], 'show') }}" id="app" data-parent="#sidebar-menu">
         <div class="sub-menu">
 
-          <li class="{{active(['admin.category.*'])}}">
+          <li class="{{active(['admin.category'], 'active')}}">
             <a class="sidenav-item-link" href="{{ route('admin.category') }}">
               <span class="nav-text">Category</span>
 
             </a>
           </li>
 
-          <li>
+          <li class="{{active(['admin.unit'], 'active')}}">
             <a class="sidenav-item-link" href="{{ route('admin.unit') }}">
               <span class="nav-text">Unit</span>
 
             </a>
           </li>
 
-          <li>
+          <li class="{{active(['admin.sales-type'], 'active')}}">
             <a class="sidenav-item-link" href="{{ route('admin.sales-type') }}">
               <span class="nav-text">Sales type</span>
 
             </a>
           </li>
 
-          <li>
+          <li class="{{active(['admin.purchase-type'], 'active')}}">
             <a class="sidenav-item-link" href="{{ route('admin.purchase-type') }}">
               <span class="nav-text">Purchase type</span>
 
             </a>
           </li>
 
-          <li>
+          <li class="{{active(['admin.product'], 'active')}}">
             <a class="sidenav-item-link" href="{{ route('admin.product') }}">
               <span class="nav-text">Product</span>
 
             </a>
           </li>
 
-          <li>
+          <li class="{{active(['admin.supplier'], 'active')}}">
             <a class="sidenav-item-link" href="{{ route('admin.supplier') }}">
               <span class="nav-text">Distributor</span>
 
