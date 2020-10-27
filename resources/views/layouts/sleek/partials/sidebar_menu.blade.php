19b3d5caf2af28ea302b2b6050e72e232b5768cf
zendzo <zendzzo@gmail.com>
2020-10-27 09:58:34 +0700

    add more list items on sidebar
    Signed-off-by: zendzo <zendzzo@gmail.com>

--- resources/views/layouts/sleek/partials/sidebar_menu.blade.php
+++ resources/views/layouts/sleek/partials/sidebar_menu.blade.php
@@ -6,8 +6,8 @@
     <li class="has-sub">
       <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#dashboard"
         aria-expanded="false" aria-controls="dashboard">
-        <i class="mdi mdi-view-dashboard-outline"></i>
-        <span class="nav-text">Dashboard</span> <b class="caret"></b>
+        <i class="mdi mdi-cart"></i>
+        <span class="nav-text">Transaksi</span> <b class="caret"></b>
       </a>
       <ul class="collapse" id="dashboard" data-parent="#sidebar-menu">
         <div class="sub-menu">
@@ -20,7 +20,7 @@
           </li>
 
           <li>
-          <a class="sidenav-item-link" href="{{ route('admin.purchase') }}">
+            <a class="sidenav-item-link" href="{{ route('admin.purchase') }}">
               <span class="nav-text">Pembelian</span>
               <span class="badge badge-success">IN</span>
             </a>
@@ -33,7 +33,7 @@
       <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#app"
         aria-expanded="false" aria-controls="app">
         <i class="mdi mdi-pencil-box-multiple"></i>
-        <span class="nav-text">DATA</span> <b class="caret"></b>
+        <span class="nav-text">MASTER DATA</span> <b class="caret"></b>
       </a>
       <ul class="collapse" id="app" data-parent="#sidebar-menu">
         <div class="sub-menu">
@@ -77,6 +77,52 @@
             <a class="sidenav-item-link" href="{{ route('admin.supplier') }}">
               <span class="nav-text">Distributor</span>
 
+            </a>
+          </li>
+
+        </div>
+      </ul>
+    </li>
+    <li class="has-sub">
+      <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#user"
+        aria-expanded="false" aria-controls="user">
+        <i class="mdi mdi-account-group"></i>
+        <span class="nav-text">User</span> <b class="caret"></b>
+      </a>
+      <ul class="collapse" id="user" data-parent="#sidebar-menu">
+        <div class="sub-menu">
+
+          <li>
+            <a class="sidenav-item-link" href="{{ route('admin.sales') }}">
+              <i class="mdi mdi-account"></i>
+              <span class="nav-text" style="margin-left: 5px;">User</span>
+              <span class="badge badge-success">OUT</span>
+            </a>
+          </li>
+
+        </div>
+      </ul>
+
+    <li class="has-sub">
+      <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#report"
+        aria-expanded="false" aria-controls="report">
+        <i class="mdi mdi-file-document-box-multiple"></i>
+        <span class="nav-text">Laporan</span> <b class="caret"></b>
+      </a>
+      <ul class="collapse" id="report" data-parent="#sidebar-menu">
+        <div class="sub-menu">
+
+          <li>
+            <a class="sidenav-item-link" href="{{ route('admin.sales') }}">
+              <span class="nav-text">Penjualan</span>
+              <span class="badge badge-warning">OUT</span>
+            </a>
+          </li>
+
+          <li>
+            <a class="sidenav-item-link" href="{{ route('admin.purchase') }}">
+              <span class="nav-text">Pembelian</span>
+              <span class="badge badge-success">IN</span>
             </a>
           </li>
 
